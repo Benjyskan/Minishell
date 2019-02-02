2ee04c3dd37393d4b68ba3cfa222fbb0d69a143e
Piriou ENZO <penzo@student.42.fr>
2019-02-02 22:42:21 +0100

    added notes.txt, 'cd -xx' now dislay usage

--- srcs/cd.c
+++ srcs/cd.c
@@ -9,21 +9,37 @@ void	cd_not_found(char **args, char **env)
 	ft_putendl_fd(": No such file or directory", 2);
 }
 
+void	cd_invalid_option(char **args, t_myenv *my_env)
+{
+	(void)my_env;
+	ft_putstr_fd(SHELL_NAME, 2);
+	ft_putstr_fd(": cd: ", 2);
+	ft_putstr_fd(args[1], 2);
+	ft_putendl_fd(" invalid option", 2);
+	ft_putendl_fd("cd: usage: cd [dir]", 2);
+}
+
 void	cd_dash(char **args, t_myenv *my_env)
 {
-	chdir(get_line_from_env("OLDPWD", my_env->envp));
-	ft_putendl(get_line_from_env("OLDPWD", my_env->envp));
+	if (ft_strcmp(args[1], "-") == 0)
+	{
+		//what if no OLDPWD ?
+		chdir(get_line_from_env("OLDPWD", my_env->envp));
+		ft_putendl(get_line_from_env("OLDPWD", my_env->envp));
+	}
+	else
+		cd_invalid_option(args, my_env);
 }
 
 int		cd_arg(char **args, t_myenv *my_env)
 {
 	if (chdir(args[1]) == -1)
 	{
+		//expand '-'
+		//so i should change cd_not_found args
 		cd_not_found(args, my_env->envp);
 		return (1);
 	}
-	//else
-	//	printf("%s\n", args[1]);
 	return (0);
 }
 
@@ -51,8 +67,9 @@ int		my_cd(char **args, t_myenv *my_env)
 		}
 		return (0);
 	}
-	else if (args[1] && ft_strcmp(args[1], "-") == 0)
-		cd_dash(args, my_env);
+	//else if (args[1] && ft_strncmp(args[1], "-", 1) == 0)//bof
+	else if (args[1] && args[1][0] == '-')//bof
+		cd_dash(args, my_env);//i can put this line in the statement above
 	else if (args[1] && cd_arg(args, my_env))//bof
 		return (1);
 	return (0);
