e9ed50a4c424d4b9756c4d00af94dd706fb9205e
Piriou ENZO <penzo@student.42.fr>
2019-02-13 18:46:25 +0100

    cd now work with ~

--- srcs/cd.c
+++ srcs/cd.c
@@ -37,6 +37,31 @@ int		cd_arg(char **args, t_myenv *my_env)
 	return (0);
 }
 
+void	cd_tilde(char **args, t_myenv *my_env)
+{
+	char	home_save[PATH_MAX];
+	int		home_size;
+	int		i;
+
+	if (!get_line_from_env("HOME", my_env->envp))
+	{
+		ft_putendl_fd("No $home variable set.", 2);
+		return ;
+	}
+	i = 1;
+	ft_bzero(home_save, PATH_MAX);
+	ft_strcpy(home_save, get_line_from_env("HOME", my_env->envp));
+	home_size = ft_strlen(home_save);
+	while (args[1][i])
+	{
+		home_save[home_size] = args[1][i];
+		home_size++;
+		i++;
+	}
+	if (chdir(home_save) == -1)
+		cd_not_found_str(home_save);
+}
+
 /*
 ** cd -: cd to OLD_PWD (and print the absolute path ?)
 ** cd : cd to ~
@@ -50,13 +75,8 @@ int		cd_arg(char **args, t_myenv *my_env)
 //should i use my struct, or create a mini_env ?
 int		my_cd(char **args, t_myenv *my_env)
 {
-	if (!args[1] || ft_strcmp(args[1], "~") == 0)
-	{
-		//TODO: Expand ~
-		if (chdir(get_line_from_env("HOME", my_env->envp)) == -1)
-			cd_not_found_str(get_line_from_env("HOME", my_env->envp));
-		//return (0);
-	}
+	if (!args[1] || args[1][0] == '~')
+		cd_tilde(args, my_env);
 	else if (args[1] && args[1][0] == '-')//bof
 		cd_dash(args, my_env);//i can put this line in the statement above
 	else if (args[1] && cd_arg(args, my_env))//bof
